161838c017e7f1d6a19241af6db7a58c7ca9afbe
Jaswanth Singh Kumar Lankadasu <jaswanthlsvyj@gmail.com>
2023-10-28 18:24:23 +0530

    ml model updated

--- output/views.py
+++ output/views.py
@@ -1,52 +1,91 @@
 # accounts/views.py
 from django.shortcuts import render, redirect
-import joblib
+import os
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
 from nltk.corpus import stopwords
 
+# Construct the absolute file path using the STATIC_ROOT setting
+csv_file_path = os.path.join('static', 'csv_files', 'AyurBharat_DataSet.csv')
 
-def model_output(user_symptoms):
-    # Load the combined models from the joblib file
-    combined_models = joblib.load("{ static 'models/ml_model.joblib' } ")
+# Read the CSV file
+data = pd.read_csv(csv_file_path)
 
-    # Access each model from the combined file
-    clf_disease = combined_models['disease_classifier']
-    clf_description = combined_models['description_classifier']
-    clf_symptoms = combined_models['symptoms_classifier']
-    clf_remedy = combined_models['remedy_classifier']
-    clf_ingredients = combined_models['ingredients_classifier']
-    clf_preparation = combined_models['preparation_classifier']
-    vectorizer=combined_models['vectorizer']
+# Preprocess text data
+stop_words = set(stopwords.words('english'))
 
-    # Define a function to preprocess text
-    stop_words = set(stopwords.words('english'))
+def preprocess_text(text):
+    text = text.lower()
+    text = ' '.join([word for word in text.split() if word not in stop_words])
+    text = ''.join([char for char in text if char.isalnum() or char.isspace()])
+    return text
 
-    def preprocess_text(text):
-        text = text.lower()
-        text = ' '.join([word for word in text.split() if word not in stop_words])
-        text = ''.join([char for char in text if char.isalnum() or char.isspace()])
-        return text
+# Define target variables for each column by their names
+y_disease = data["Disease_Ayurvedic_Name"]
+y_description = data["Description"]
+y_symptoms = data["Symptoms"]
+y_remedy = data["Ayurvedic_Remedy"]
+y_ingredients = data["Ingredients"]
+y_preparation = data["Preparation"]
 
-    user_symptoms = preprocess_text(user_symptoms)
+# Split data into features (X) and target (y) for each column
+X = data["Symptoms"]
 
-    # Vectorize user input (symptoms)
-    user_input_vectorized = vectorizer.transform([user_symptoms])
+# Vectorize text data using TF-IDF
+vectorizer = TfidfVectorizer()
+X_vectorized = vectorizer.fit_transform(X)
 
-    # Make predictions using the loaded models
-    predicted_disease = clf_disease.predict(user_input_vectorized)[0]
+# Train a RandomForestClassifier for each column
+clf_disease = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_disease.fit(X_vectorized, y_disease)
+
+clf_description = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_description.fit(X_vectorized, y_description)
+
+clf_symptoms = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_symptoms.fit(X_vectorized, y_symptoms)
+
+clf_remedy = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_remedy.fit(X_vectorized, y_remedy)
+
+clf_ingredients = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_ingredients.fit(X_vectorized, y_ingredients)
+
+clf_preparation = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_preparation.fit(X_vectorized, y_preparation)
+
+def predict_disease_info( symptoms ):
+    # Vectorize user input (symptoms only)
+    symptoms = preprocess_text(symptoms)
+    user_input_vectorized = vectorizer.transform([symptoms])
+
+    # Predict Disease_Ayurvedic_Name
+    disease_prediction = clf_disease.predict(user_input_vectorized)[0]
+
+    # Predict Description
     description_prediction = clf_description.predict(user_input_vectorized)[0]
+
+    # Predict Symptoms
     symptoms_prediction = clf_symptoms.predict(user_input_vectorized)[0]
+
+    # Predict Ayurvedic_Remedy
     remedy_prediction = clf_remedy.predict(user_input_vectorized)[0]
+
+    # Predict Ingredients
     ingredients_prediction = clf_ingredients.predict(user_input_vectorized)[0]
+
+    # Predict Preparation
     preparation_prediction = clf_preparation.predict(user_input_vectorized)[0]
 
     return {
-        'predicted_disease' : predicted_disease,
-        'description_prediction' : description_prediction,
-        'symptoms_prediction' : symptoms_prediction,
-        'remedy_prediction' : remedy_prediction,
-        'ingredients_prediction' : ingredients_prediction,
-        'preparation_prediction' : preparation_prediction
-    }
+            'disease_prediction': disease_prediction,
+            'description_prediction': description_prediction,
+            'symptoms_prediction': symptoms_prediction,
+            'remedy_prediction': remedy_prediction,
+            'ingredients_prediction': ingredients_prediction,
+            'preparation_prediction': preparation_prediction
+        }
 
 
 def inputpage(request):
@@ -54,22 +93,15 @@ def inputpage(request):
 
 def outputpage(request):
     if request.method == 'POST' :
-        symptoms = 'This is the trail symptoms'
-        disease = 'This is trail disease name'
-        Description = 'This is an trail Description'
-        Remedy = 'This is a remedy'
-
-        if  (request.POST.get('disease')):
-            disease = request.POST['disease']
-
-        if (request.POST.get('symptoms')):
-            symptoms = request.POST['symptoms']
+        symptoms = request.POST.get('symptoms')  # Get user input
             
-        # predictions = []        
-        # predit = {'disease_name': disease , 'Symptoms': symptoms , 'Description' : Description , 'Remedy': Remedy}
-        # predictions.append(predit)
+        predictions = []        
+        predit = predict_disease_info(symptoms)
+        predictions.append(predit)
 
-        return render(request, 'outputpage.html', {'disease_name': disease , 'Symptoms': symptoms , 'Description' : Description , 'Remedy': Remedy} )
+        print(symptoms, predictions)
+
+        return render(request, 'outputpage.html', { 'predictions': predictions } )
     
     return redirect( 'inputpage')
 
