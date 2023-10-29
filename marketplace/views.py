0f6bc8de6454f18ab1ac154fc1131f8be3de7955
Jaswanth Singh Kumar Lankadasu <jaswanthlsvyj@gmail.com>
2023-10-29 22:49:48 +0530

    changing

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def ayurmart(request):
+    return render(request, 'ayurMart.html')
+
+def ayurdoctor(request):
+    return render(request, 'ayurDoctor.html')
