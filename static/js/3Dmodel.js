5a2e5e3ebaaf114c0dc10cd59c46ac0d669829d2
komalpasumarthy <92156651+komalpasumarthy@users.noreply.github.com>
2023-10-29 17:33:20 +0530

    Updated 3D model.js

--- static/js/3Dmodel.js
+++ static/js/3Dmodel.js
@@ -4,48 +4,6 @@ import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/thr
 import { GLTFLoader } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/loaders/GLTFLoader.js';
 
 
-/* window.onload = function () {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 2); // Adjust the camera position
-    camera.lookAt(0, 0, 0);       // Adjust the camera look-at target
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('model-container').appendChild(renderer.domElement);
-
-    const loader = new GLTFLoader();
-
-    // Add proper lighting
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    scene.add(directionalLight);
-
-    // Load the .glb model and add it to the scene
-    loader.load("{% static 'models/3dModelExpV2.glb' %}", (gltf) => {
-        const model = gltf.scene;
-
-        scene.add(model);
-
-         // Start the rendering loop
-        console.log("Function Triggered");
-
-        function animate() {
-            requestAnimationFrame(animate);
-
-            model.scale.set(0.1, 0.1, 0.1); // Adjust the scale
-            model.position.set(0, 0, 0);     // Adjust the position
-
-            // Add any model animations or interactions here
-            model.rotation.x += 0.01;
-            model.rotation.y += 0.01;
-
-            renderer.render(scene, camera);
-        }
-        animate();
-    });
-};
-*/
-
 // Create a scene
 const scene = new THREE.Scene();
 
