04aef53e35a45a046fa751cdfe617a01850d136f
Jaswanth Singh Kumar Lankadasu <jaswanthlsvyj@gmail.com>
2023-12-19 15:56:23 +0530

    stopwords warning removed

--- output/views.py
+++ output/views.py
@@ -8,6 +8,17 @@ from nltk.corpus import stopwords
 import nltk
 import json
 
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('stopwords')
+
 # Construct the absolute file path using the STATIC_ROOT setting
 csv_file_path = os.path.join('static', 'csv_files', 'AyurBharat_DataSet.csv')
 
